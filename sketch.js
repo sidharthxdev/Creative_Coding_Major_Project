5b988912f27e62641d1ecd5b7accb9c08f85291b
KristienY <jyua0832@uni.sydney.edu.au>
2025-06-02 23:24:05 +1000

    basic shape first

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  noLoop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw();
 }
 
 function draw() {
-  background(220);
+
+  noStroke();
+  
+  fill("blue")
+  rect(0, 0, 400, 300)
+  
+  fill("#F71B0B")
+  rect(350, 0, 400, 250)
+  
+
+  fill("purple")
+  rect(500, 200, 250, 450)
+  
+
+  fill("yellow")
+  rect(0, 300, 350, 440)
+  
+
+  fill("#FF9800")
+  rect(200, 400, 400, 250)
+  
+  fill("#E91E63")
+  rect(200, 200, 400, 200)
+
+  fill("#E91E63")
+  ellipse(200, 270, 350, 180);
+  
 }
