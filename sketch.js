8e17cc10c231571e2030a50ee9a03b836602bf31
KristienY <jyua0832@uni.sydney.edu.au>
2025-06-06 21:39:52 +1000

    add circle 2

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
   drawBackground();
   drawCircleKristien1();
-  drawCircleKristien2();
+  drawCircleKristien2(0, 350, 100, 65);
   drawCircleCola1();
   drawCircleCola2();
   drawCircleYin1();
@@ -277,6 +277,93 @@ function drawSpiral(cx, cy, k, maxAngle) {
 }*/
 
 
-function drawCircleKristien2(){
+function drawCircleKristien2(x, y, w, h){
+// I chose to draw an eye because the artist has a multicultural background. To me,the eye represents observation, identity, and perception across different cultures. It's a symbol of seeing the world through multiple lenses — personal, cultural, and spiritual.
+let cx = x + w / 2;
+  let cy = y + h / 2;
+  let left = x;
+  let right = x + w;
+  let top = y;
+  let bottom = y + h;
+  
+/*circle
+  fill("#FFA726");
+  noStroke();
+  circle(x + w / 2, y + h / 2, max(w, h) + 50);
+*/
+  
+//shapes around eyes
+  let maxLayers = 7; // wanna 7 layer
+  let startR = 80; // outer R
+  let layerGap = 10; // 
+  
+  for (let layer = 0; layer < maxLayers; layer++) {
+  let r = startR - layer * layerGap; 
+  let count = 40 + layer * 5; 
+  
+  for (let i = 0; i < count; i++) {
+    let angle = TWO_PI * i / count;
+    let x = cx + r * cos(angle);
+    let y = cy + r * sin(angle);
 
+    if (i % 3 === 0) {
+      fill('#e76f51');
+      noStroke();
+      circle(x, y, 8);
+    } else if (i % 3 === 1) {
+      fill('#90d4ff');
+      noStroke();
+      rect(x - 4, y - 4, 8, 8);
+    } else {
+      fill('#2c75ff');
+      noStroke();
+      triangle(x, y - 5, x + 5, y + 5, x - 5, y + 5);
+    }
+  }
+
+// outer eye
+  fill('#yellow');
+  stroke('#111111');     
+  strokeWeight(4); 
+  beginShape();
+  // up line
+  vertex(left, cy);
+  bezierVertex(cx - w * 0.2, top, cx + w * 0.2, top, right, cy);
+  // got inspiration from website p5.js moon shape: https://p5js.org/reference/p5/bezierVertex/
+  // down line
+  bezierVertex(cx + w * 0.2, bottom, cx - w * 0.2, bottom, left, cy);
+  endShape(CLOSE);
+  //I got ChatGPT help to do math, that's too tricky for me to do all the math things :(
+
+  
+// middle
+  let eyeRadius = min(w, h) / 1.5;//Let the eyeball occupy approximately half of the eye shape's width or height.
+  drawMultiColorEyeball(cx, cy, eyeRadius);
+  
+function drawMultiColorEyeball(cx, cy, maxRadius) {
+  let colors = [
+  '#90d4ff',  
+  '#3e9efc', 
+  '#2c75ff', 
+  '#6540f5',
+  '#4b2ca7', 
+  '#2b185a', 
+  '#111111' ];
+
+  noStroke();
+  for (let i = 0; i < colors.length; i++) {
+    let r = maxRadius * (1 - i * 0.15); // reduce 15% each layer
+    fill(colors[i]); //colour list i made
+    circle(cx, cy, r);
+  }
 }
+  
+// crucifix reflects the artist’s respect for all spiritual and cultural beliefs
+  stroke('#ffffff');
+  strokeWeight(2);
+  line(cx - 4, cy, cx + 4, cy);
+  line(cx, cy - 4, cx, cy + 9); 
+      
+ }
+}
+
